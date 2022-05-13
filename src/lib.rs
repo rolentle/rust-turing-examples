b69b9a333917aac90a0d5877ec89e602716060fd
Rolen T. Le <rolentle@gmail.com>
2022-05-13 15:19:18 -0400

    iteration 3

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub struct Dish {
     category: String,   
 }
 
+#[derive(Clone)]
 pub struct Potluck {
     date: String,
     dishes: Vec<Dish>,
@@ -19,6 +20,10 @@ impl Potluck {
 
     pub fn add_dish(&mut self, dish: Dish) {
         self.dishes.push(dish);
+    }
+
+    pub fn get_all_from_category(self, category: String) -> Vec<Dish> {
+        self.dishes.into_iter().filter(|dish| dish.category == category).collect()
     }
 }
 
@@ -47,6 +52,22 @@ mod tests {
         let cocktail_meatballs = Dish { name: "Cocktail Meatballs".to_string(), category: "entree".to_string() };
         potluck.add_dish(cocktail_meatballs.clone());
         assert_eq!(potluck.dishes.len(), 2);
+    }
+
+    #[test]
+    fn iteration_3() {
+        let mut potluck = Potluck::new("7-13-18".to_string());
+        let couscous_salad = Dish { name: "Couscous Salad".to_string(), category: "appetizer".to_string() };
+        let summer_pizza = Dish { name: "Summer Pizza".to_string(), category: "appetizer".to_string() };
+        let cocktail_meatballs = Dish { name: "Cocktail Meatballs".to_string(), category: "entree".to_string() };
+        let candy_salad = Dish { name: "Candy Salad".to_string(), category: "dessert".to_string() };
+        potluck.add_dish(couscous_salad);
+        potluck.add_dish(summer_pizza);
+        potluck.add_dish(cocktail_meatballs);
+        potluck.add_dish(candy_salad);
+        let potluck_length = potluck.clone().get_all_from_category("appetizer".to_string()).len();
+        assert_eq!(potluck_length, 2);
+        assert_eq!(potluck.get_all_from_category("appetizer".to_string()).first().unwrap().name, "Couscous Salad".to_string());
     }
 }
 
